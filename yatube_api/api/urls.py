e487284c812c348be2e16da96364681d533fe3e9
helzzx! <mvmnth@gmail.com>
2025-04-10 17:55:09 +0500

    233

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,20 +1,18 @@
-from rest_framework.routers import DefaultRouter
-from rest_framework_nested import routers
-from django.urls import include, path
-from api.views import PostViewSet, GroupViewSet, CommentViewSet
-from rest_framework.authtoken import views
+from django.urls import path, include
+from rest_framework import routers
+from rest_framework.authtoken import views as token_views
+from . import views
 
-
-router = DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('groups', GroupViewSet)
-
-
-posts_router = routers.NestedSimpleRouter(router, 'posts', lookup='post')
-posts_router.register('comments', CommentViewSet, basename='post-comments')
+router = routers.SimpleRouter()
+router.register('posts', views.PostViewSet)
+router.register('groups', views.GroupViewSet)
+router.register(
+    r'posts/(?P<post_pk>\d+)/comments',
+    views.CommentViewSet,
+    basename='post-comments'
+)
 
 urlpatterns = [
-    path('api-token-auth/', views.obtain_auth_token),
-    path('', include(router.urls)),
-    path('', include(posts_router.urls)),
+    path('api-token-auth/', token_views.obtain_auth_token),
+    path('', include(router.urls))
 ]
